65f5c42b13f8ae4b165b02a62b175db165f44888
dewnavapan <125631063+dewnavapan@users.noreply.github.com>
2023-05-05 14:05:00 +0700

    Initial commit

---
+++ src/components/prop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-model="text" />
+    <button @click="gotoHome()">GO</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    gotoHome() {
+      this.$router.push({ path: "/", query: { msg: this.text } });
+    },
+  },
+};
+</script>
